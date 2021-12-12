cf6145f2c4746cb040646f0f517c3ccc13ec398c
KJ Kim <msrndsy@gmail.com>
2021-12-12 10:59:19 +0000

    DB Connection

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql2')
+var config = require('../config/config');
+
+var connection = mysql.createConnection(config['database'])
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +11,34 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/test/', function(req, res, next) {
+  console.log('loglog');
   res.json([{answer: 'OK'}]);
 });
+
+router.get('/dbtest/', function(req, res, next) {
+  console.log(' Try DB Connection');
+  connection.connect()
+
+  connection.query('SELECT * FROM VOCA', function (err, rows, fields) {
+    if (err) {
+      console.log('DB Connection err');
+      throw err
+    } else {
+      console.log('DB Connection Success');
+      console.log("test" + rows[0].vocaListId);
+      res.send(rows);
+    
+    }
+    // console.log('The solution is: ', rows[0].solution)
+    // console.log(err);
+
+    // console.log(fields);
+  })
+  
+
+  connection.end()
+});
+
+
 
 module.exports = router;
